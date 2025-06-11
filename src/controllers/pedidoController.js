094c7082e4657dc1de51d8af37bceaa946d3aff5
Lucas <lucas_rodriguesguerra@unochapeco.edu.br>
2025-06-10 22:36:36 -0300

    feat: melhorias em comanda

--- src/controllers/pedidoController.js
+++ src/controllers/pedidoController.js
@@ -32,6 +32,40 @@ const get = async (req, res) => {
   }
 };
 
+const getProdutoByIdToken = async (req, res) => {
+  try {
+    const idToken = req.params.idToken ? req.params.idToken.toString().replace(/\D/g, '') : null;
+
+    if (!idToken || Number.isNaN(Number(idToken))) {
+      return res.status(400).send({
+        message: 'ID da categoria é obrigatório e deve ser um número válido.',
+      });
+    }
+
+    const response = await PedidoModel.findAll({
+      where: {
+        idToken,
+      },
+    });
+
+    if (!response || response.length === 0) {
+      return res.status(404).send({
+        message: 'Nenhum produto encontrado para esta categoria.',
+      });
+    }
+
+    return res.status(200).send({
+      message: 'Dados encontrados!',
+      response,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      message: 'Ops! Ocorreu um erro no servidor.',
+      response: error.message,
+    });
+  }
+};
+
 const create = async (req, res) => {
   try {
     const {
@@ -135,6 +169,7 @@ const destroy = async (req, res) => {
 
 export default {
   get,
+  getProdutoByIdToken,
   create,
   update,
   destroy,
