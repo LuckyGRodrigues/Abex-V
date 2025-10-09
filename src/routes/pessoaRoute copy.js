f3ca6e9c0d88f7c5ed68e952ba797753107a8484
Rudivan060 <rudivan395e@gmail.com>
2025-10-09 08:52:40 -0300

    correção crud orçamento
    eu sem querer criei uma copia do pessoaRoute

--- src/routes/pessoaRoute copy.js
+++
@@ -1,9 +0,0 @@
-import controller from '../controllers/pessoaController';
-
-export default (app) => {
-  app.get('/pessoa', controller.get);
-  app.get('/pessoa/:id', controller.get);
-  app.post('/pessoa/create', controller.create);
-  app.patch('/pessoa/update/:id', controller.update);
-  app.delete('/pessoa/delete/:id', controller.destroy);
-};
